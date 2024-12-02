17adfcac15fd5e3f03e9d2d0e743b51b67c31dd4
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-12-02 14:14:22 +0100

    feat: add new components and types for user and progress loading features

---
+++ fusion-portal/react/components/src/components/index.ts
@@ -0,0 +1,8 @@
+export * from './skeleton/Skeleton';
+export { User } from './user/UserCard';
+
+export { Message } from './message/Message';
+export * from "./profile-card-header/ProfileCardHeader";
+export * from ".//progress-loader";
+export * from "./info-icon/InfoIcon"
+
