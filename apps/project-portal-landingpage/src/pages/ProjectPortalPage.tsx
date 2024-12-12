f3ba648dbda46f0dd3c60bc97b871d1b8d2eea00
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-12-12 09:26:46 +0100

    chore: update project-portal-landingpage version to 0.0.35; remove unused imports and refactor portal test code

--- apps/project-portal-landingpage/src/pages/ProjectPortalPage.tsx
+++ apps/project-portal-landingpage/src/pages/ProjectPortalPage.tsx
@@ -2,7 +2,7 @@ import { ProjectHeader } from '../components/page-header/PageHeader';
 
 import styled from 'styled-components';
 
-import { Card, Typography } from '@equinor/eds-core-react';
+import { Typography } from '@equinor/eds-core-react';
 
 import { User } from '@equinor/fusion-portal-react-components';
 import { useNavigateOnContextChange, ProjectPortalInfoBox } from '@equinor/project-portal-common';
@@ -10,11 +10,6 @@ import { useNavigateOnContextChange, ProjectPortalInfoBox } from '@equinor/proje
 import { PortalContextSelector } from '../components/context/PortalContextSelector';
 import { Allocations } from '../components/allocations/Allocations';
 import { useFeature } from '@equinor/fusion-framework-react-app/feature-flag';
-
-import { useFramework } from '@equinor/fusion-framework-react';
-
-import { useQuery } from '@tanstack/react-query';
-import { useAppModules } from '@equinor/fusion-framework-react-app';
 
 export const Styles = {
     Wrapper: styled.main`
@@ -54,28 +49,9 @@ type Portal = {
     shortName: string;
 };
 
-const usePortals = () => {
-    const modules = useAppModules();
-    const client = modules.http.createClient('portal-client');
-
-    return useQuery({
-        queryKey: ['portal-list'],
-        queryFn: async () => {
-            try {
-                const res = await client.fetch(`api/portals`);
-                if (!res.ok) throw res;
-                return (await res.json()) as Portal[];
-            } catch (error) {
-                console.error('Failed to fetch portals', error);
-                return [] as Portal[];
-            }
-        },
-    });
-};
-
 export const ProjectPortalPage = (): JSX.Element => {
     const { feature } = useFeature('project-prediction');
-    const { data, error, isLoading } = usePortals();
+
     useNavigateOnContextChange();
     return (
         <Styles.Wrapper>
@@ -97,39 +73,6 @@ export const ProjectPortalPage = (): JSX.Element => {
                         <PortalContextSelector />
                     </Styles.ContentWrapper>
                     {feature?.enabled && <Allocations />}
-                    <div>
-                        {isLoading && <div>Loading...</div>}
-                        {error && <div>Error: {error.message}</div>}
-                        <div style={{ display: 'flex', gap: '1rem' }}>
-                            {data
-                                ?.filter((p) => p.name.includes('ProCoSys'))
-                                .sort((a, b) => (a.name > b.name ? -1 : 1))
-                                .map((portal: any) => (
-                                    <Card key={portal.id}>
-                                        <a
-                                            href={`https://${portal.shortName.toLowerCase()}-pcs5-demo-test.radix.equinor.com/`}
-                                            style={{
-                                                padding: '1rem',
-                                                display: 'flex',
-                                                gap: '1rem',
-                                                height: '60px',
-                                                textDecoration: 'none',
-                                            }}
-                                        >
-                                            <div
-                                                dangerouslySetInnerHTML={{ __html: portal.icon }}
-                                            />
-                                            <div>
-                                                <Typography variant="h2">
-                                                    {portal.shortName}
-                                                </Typography>
-                                                <Typography>{portal.description}</Typography>
-                                            </div>
-                                        </a>
-                                    </Card>
-                                ))}
-                        </div>
-                    </div>
                 </Styles.Content>
             </ProjectHeader>
         </Styles.Wrapper>
